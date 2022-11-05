06ef1e976edc9377e3024b15e419fdf49cff853c
Renan Cesar <cesar.renan@hotmail.com>
2022-11-05 17:50:28 -0300

    Alterando rotas para fazer dois estilos

--- src/components/Conteudo/index.jsx
+++ src/components/Conteudo/index.jsx
@@ -1,9 +1,15 @@
+import Footer from 'components/Footer'
+import Menu from 'components/Menu'
+import PopCorn from 'components/PopCorn'
 import { Outlet } from 'react-router-dom'
 
 export default function Conteudo() {
   return (
     <main>
+      <Menu />
       <Outlet />
+      <PopCorn />
+      <Footer />
     </main>
   )
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Menu from './components/Menu'
 import Home from './pages/Home'
 import Sugestao from './pages/Sugestao'
 import PaginaPadrao from './components/PaginaPadrao'
@@ -7,28 +6,23 @@ import TopFilmes from 'pages/TopFilmes'
 import TopSeries from 'pages/TopSeries'
 import Error from 'pages/Error'
 import Conteudo from 'components/Conteudo'
-import PopCorn from 'components/PopCorn'
-import Footer from 'components/Footer'
 
 function AppRoutes() {
   return (
     <>
       <BrowserRouter>
-        <Menu />
         <Routes>
           <Route path="/" element={<PaginaPadrao />}>
             <Route path="/" element={<Home />} />
-            <Route path="/Sugestao" element={<Sugestao />} />
-          </Route>
-          <Route path="/" element={<Conteudo />}>
             <Route path="/TopFilmes" element={<TopFilmes />} />
             <Route path="/TopSeries" element={<TopSeries />} />
+          </Route>
+          <Route path="/" element={<Conteudo />}>            
+          <Route path="/Sugestao" element={<Sugestao />} />
           </Route>
 
           <Route path="*" element={<Error/>}/>
         </Routes>
-        <PopCorn/>
-        <Footer />
       </BrowserRouter>
     </>
   )
