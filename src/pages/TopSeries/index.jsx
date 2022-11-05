32fd218b6d0207d2e60f6b8911f03ea549cc6856
Renan Cesar <cesar.renan@hotmail.com>
2022-11-05 10:23:00 -0300

    Colocando carousel em séries

--- src/pages/TopSeries/index.jsx
+++ src/pages/TopSeries/index.jsx
@@ -1,17 +1,38 @@
+import { useRef } from 'react';
 import styles from './TopSeries.module.css';
 import SerieCard from "components/SerieCard";
 import posts from 'json/series.json';
 
 export default function TopSeries() {
+
+  const carousel = useRef(null);
+
+  const handleLeftClick = e =>{
+    e.preventDefault();
+    carousel.current.scrollLeft -= carousel.current.offsetWidth;
+  }
+
+  const handleRightClick = e =>{
+    e.preventDefault();
+    carousel.current.scrollLeft += carousel.current.offsetWidth;
+  }
+
   return(
-        <div className={styles.container}>
-          <ul className={styles.posts}>
-              {posts.map((post) => (
-                  <li key={post.id}>
-                      <SerieCard post={post} />
-                  </li>
-              ))}                
-          </ul>
-        </div>    
+    <>
+      <div className={styles.container} ref={carousel}>
+        <ul className={styles.posts}>
+            {posts.map((post) => (
+                <li key={post.id}>
+                    <SerieCard post={post} />
+                </li>
+            ))}                
+        </ul>
+      </div>
+      <div className={styles.buttonsContainer}>
+        <button className={styles.btnCarrosel} onClick={handleLeftClick}>Voltar</button>
+        <button className={styles.btnCarrosel} onClick={handleRightClick}>Avançar</button>  
+      </div>     
+    
+    </>
 );
 }
