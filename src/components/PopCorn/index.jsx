864e86a7c0da710324e1dc915b9e086f0cd6f137
Renan Cesar <cesar.renan@hotmail.com>
2022-11-06 17:44:26 -0300

    Resolvendo bug da area clicável da popcorn

--- src/components/PopCorn/index.jsx
+++ src/components/PopCorn/index.jsx
@@ -3,18 +3,16 @@ import PopImage from '../../assets/PopImage/popcorn-suges.png';
 import { Link } from 'react-router-dom';
 
 export default function PopCorn() {
-    return(
-        <Link to="/Sugestao">
-
-        <div className={styles.position}>
-            <h1 className={styles.title}>Sujestões?</h1>
-            <img
-            className={styles.image} 
-            src={PopImage} 
-            alt="Pop Corn" 
-            />
-        </div>
-
-        </Link>
+    return(  
+            <div className={styles.position}>
+                <h1 className={styles.title}>Sujestões?</h1>
+                <Link to="/Sugestao">
+                    <img
+                    className={styles.image} 
+                    src={PopImage} 
+                    alt="Pop Corn" 
+                    />
+                </Link>
+            </div>
     )
 }
