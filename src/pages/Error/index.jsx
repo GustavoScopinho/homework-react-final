6b816c4235befe44bd8e4e354ccfa0b5accafe1e
onlyxdev <progdev.aula@gmail.com>
2022-11-06 12:20:31 -0300

    ReadMe + Menu Adjust

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -15,7 +15,7 @@ export default function Error() {
 
             <div className={styles.error}>
                 <h1>ERROR!</h1>
-                <h5>Não sabemos se foi culpa sua ou nossa...(com certeza sua, nós raramente erramos). Mas por favor, clique no botão abaixo parar ser redirecionado ao Início!</h5>
+                <h5>Não sabemos se foi culpa sua ou nossa...(com certeza sua, nós raramente erramos). Mas por favor, clique no botão abaixo para ser redirecionado ao Início!</h5>
                 <button className={styles.errorbtn} onClick={GoHome}>Início</button>
             </div>
         </div>
