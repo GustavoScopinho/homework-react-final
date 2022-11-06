5fb74f41c717d3f1e6fac1e84b03f703bca3d25e
Renan Cesar <cesar.renan@hotmail.com>
2022-11-06 18:05:01 -0300

    Resolvendo conflito

--- src/components/Conteudo/index.jsx
+++ src/components/Conteudo/index.jsx
@@ -1,8 +1,9 @@
-import FooterSugestao from 'components/FooterSugestao'
+
 import styles from './Conteudo.module.css';
 import MenuSugestao from 'components/MenuSugestao'
 import PopCorn from 'components/PopCorn'
 import { Outlet } from 'react-router-dom'
+import FooterSugestao from 'components/FooterSugestao';
 
 export default function Conteudo() {
   return (
